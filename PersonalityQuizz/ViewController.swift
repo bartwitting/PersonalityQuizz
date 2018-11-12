e1504a5bf5d622ea39b089f9f35c2c2528758fba
bartwitting <bartwitting@icloud.com>
2018-11-13 00:28:47 +0100

    Initial Commit

---
+++ PersonalityQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuizz
+//
+//  Created by Bart Witting on 13/11/2018.
+//  Copyright © 2018 Bart Witting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
